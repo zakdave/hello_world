750071fb6ae575366f6e7e854d692cc8a641b323
Garrett Dancik <garrett.dancikg@gmail.com>
2023-10-30 23:14:18 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# hello_world (test)
+
+A sample repository for a classroom assignment, to help students learn Github commands
+
---
+++ hello.java
@@ -0,0 +1,17 @@
+// a simple program to practice common Github commands 
+
+public class hello {
+
+    public static void main(String [] args) {
+
+       // initialize variables
+       String firstName = "Alex";
+
+       // delete this line
+       System.out.println("Modify this code");
+	
+       // Using the value of the variable above, output a message in the format, "Hello, I am Alex".
+
+    }
+}
+

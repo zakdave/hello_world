8b4e160d4213280042e34d72e00f20a810f626c1
Zachary Davidson <zachary.j.davidson@gmail.com>
2024-03-21 10:25:49 -0400

    Update hello.java
    adds name variable and output

--- hello.java
+++ hello.java
@@ -5,10 +5,10 @@ public class hello {
     public static void main(String [] args) {
 
        // initialize variables
-       String firstName = "Alex";
+       String firstName = "Zachary";
 
        // delete this line
-       System.out.println("Modify this code");
+       System.out.println("Hello, I am" + firstName);
 	
        // Using the value of the variable above, output a message in the format, "Hello, I am Alex".
 
